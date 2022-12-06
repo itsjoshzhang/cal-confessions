b5d1825a41cd2770cddabcc0f43d1f699c6d7f78
Josh Zhang <zhangjoshua360@gmail.com>
2022-12-06 15:05:43 -0800

    full reset to solution file

--- index.js
+++ index.js
@@ -16,39 +16,31 @@ async function connect() {
   await mongoose.connect('mongodb+srv://calconfessions:calconfessions@calconfessions.r6kz7bf.mongodb.net/test')
 }
 
-// Creates a new mongoose.Schema with the front as a String and the back as a String. 
 const flashcardSchema = new mongoose.Schema({
-    front: String,
-    back: String
+  front: String,
+  back: String
 });
 
-// Creates a new mongoose.model with "Confession" as the name, using the confessionSchema we just created.
-const Flashcard = new mongoose.model(flashcardSchema, "Flashcard");
+const Flashcard = mongoose.model('Flashcard', flashcardSchema)
 
-// Modified POST route to add a new document to our database. Save the confession into our database. 
 app.post("/new", async (req, res) => {
     const newCard = new Flashcard({front:req.body.front, back:req.body.back});
     await newCard.save()
     return res.send(newCard)
 })
 
-// Uses a method in Mongoose to find all the posts we have in our database. 
 app.get("/cards", async (req, res) => {
-    const foundCards = await newCard.find();
+    const foundCards = await Flashcard.find()
     return res.send(foundCards)
 })
 
-// Uses a method in Mongoose to find a post by its ID. 
 app.get("/card/:id", async (req, res) => {
-    let id = req.params.id
-    const foundCard = await newCard.findById(id);
+    const foundCard = await Flashcard.findById(req.params.id)
     return res.send(foundCard)
 })
 
-// Uses a method in Mongoose to delete a post by its ID. 
 app.get("/delete/:id", async (req, res) => {
-    let id = req.params.id
-    const foundCard = await newCard.findByIdAndDelete(id);
+    const foundCard = await Flashcard.findByIdAndDelete(req.params.id)
     return res.send(foundCard)
 })
 
