23f67a2dcd456d52258ef55f260b59458ea5f8a3
aqualilypad <119380076+wenjiasss@users.noreply.github.com>
2022-12-06 17:22:08 -0800

    heart
    heart

--- script.js
+++ script.js
@@ -135,3 +135,13 @@ async function deleteCardById(id) {
     let card = await apiCall(formattedUrl)
     return card
 }
+
+const button = document.querySelector(".heart-like-button");
+
+button.addEventListener("click", () => {
+  if (button.classList.contains("liked")) {
+    button.classList.remove("liked");
+  } else {
+    button.classList.add("liked");
+  }
+});
